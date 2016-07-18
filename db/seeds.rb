5f77ab6f2c3cec3fd0a573764f21c2edf2343757
Ahmed Yousry <ahmed.yousry@intellisc.com>
2016-07-18 22:06:46 +0200

    add seeds file entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin_user = User.find_or_initialize_by(email: 'admin@clark-case.com')
+admin_user.password = '12345678'
+admin_user.is_admin = true
+admin_user.save!
+
+seed_post = Post.find_or_initialize_by(body: 'sample seed post')
+seed_post.user = admin_user
+seed_post.save!
+
+seed_comment = Comment.find_or_initialize_by(text: 'sample seed comment')
+seed_comment.post = seed_post
+seed_comment.user = admin_user
+seed_comment.save!
